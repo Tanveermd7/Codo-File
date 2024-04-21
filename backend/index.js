efb350585bcba30245c76d2546f907f205a96a09
Tanveer <mdtanveerjohncena@gmail.com>
2024-04-21 16:18:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT ;
 
 require('./db/conn'); 
 
-app.use(cors()); 
+app.use(cors(
+    {
+        origin: ["https://codo-file-frontend.vercel.app/"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+)); 
 app.use(express.json()); 
 app.use(require('./router/auth'));
 
